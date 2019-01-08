bcf7159be8d7a04e4cbad91008bc1384bb2589e4
Pavel Vlasov <freakycue@gmail.com>
2019-01-08 21:10:20 +1100

    monorepo

--- packages/client/src/extension.ts
+++ packages/client/src/extension.ts
@@ -8,35 +8,66 @@
 
 import * as path from 'path';
 
-import { workspace, ExtensionContext, languages, extensions, Uri } from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind, NotificationType } from 'vscode-languageclient';
-import { schemaContributor, CUSTOM_SCHEMA_REQUEST, CUSTOM_CONTENT_REQUEST } from './schema-contributor'
+import {
+	workspace,
+	ExtensionContext,
+	languages,
+	extensions,
+	Uri
+} from 'vscode';
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind,
+	NotificationType
+} from 'vscode-languageclient';
+import {
+	schemaContributor,
+	CUSTOM_SCHEMA_REQUEST,
+	CUSTOM_CONTENT_REQUEST
+} from './schema-contributor';
 
 export interface ISchemaAssociations {
 	[pattern: string]: string[];
 }
 
 namespace SchemaAssociationNotification {
-	export const type: NotificationType<ISchemaAssociations, any> = new NotificationType('json/schemaAssociations');
+	export const type: NotificationType<
+		ISchemaAssociations,
+		any
+	> = new NotificationType('json/schemaAssociations');
 }
 
 namespace DynamicCustomSchemaRequestRegistration {
-	export const type: NotificationType<{}, {}> = new NotificationType('yaml/registerCustomSchemaRequest');
+	export const type: NotificationType<{}, {}> = new NotificationType(
+		'yaml/registerCustomSchemaRequest'
+	);
 }
 
 export function activate(context: ExtensionContext) {
-
 	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(path.join('node_modules', 'yaml-language-server', 'out', 'server', 'src', 'server.js'));
+	let serverModule = context.asAbsolutePath(
+		path.join(
+			'node_modules',
+			'@serverless-ide/language-server',
+			'out',
+			'server.js'
+		)
+	);
 
 	// The debug options for the server
-	let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	let serverOptions: ServerOptions = {
-		run : { module: serverModule, transport: TransportKind.ipc },
-		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+		run: { module: serverModule, transport: TransportKind.ipc },
+		debug: {
+			module: serverModule,
+			transport: TransportKind.ipc,
+			options: debugOptions
+		}
 	};
 
 	// Options to control the language client
@@ -48,7 +79,11 @@ export function activate(context: ExtensionContext) {
 		],
 		synchronize: {
 			// Synchronize the setting section 'languageServerExample' to the server
-			configurationSection: ['serverlessIDE', 'http.proxy', 'http.proxyStrictSSL'],
+			configurationSection: [
+				'serverlessIDE',
+				'http.proxy',
+				'http.proxyStrictSSL'
+			],
 			// Notify the server about file changes to '.clientrc files contain in the workspace
 			fileEvents: [
 				workspace.createFileSystemWatcher('**/*.?(e)y?(a)ml'),
@@ -58,7 +93,12 @@ export function activate(context: ExtensionContext) {
 	};
 
 	// Create the language client and start the client.
-	let client = new LanguageClient('yaml', 'Yaml Support', serverOptions, clientOptions);
+	let client = new LanguageClient(
+		'yaml',
+		'Yaml Support',
+		serverOptions,
+		clientOptions
+	);
 	let disposable = client.start();
 
 	// Push the disposable to the context's subscriptions so that the
@@ -66,12 +106,15 @@ export function activate(context: ExtensionContext) {
 	context.subscriptions.push(disposable);
 
 	client.onReady().then(() => {
-		client.sendNotification(SchemaAssociationNotification.type, getSchemaAssociation(context));
+		client.sendNotification(
+			SchemaAssociationNotification.type,
+			getSchemaAssociation(context)
+		);
 		client.sendNotification(DynamicCustomSchemaRequestRegistration.type);
-		client.onRequest(CUSTOM_SCHEMA_REQUEST, (resource) => {
+		client.onRequest(CUSTOM_SCHEMA_REQUEST, resource => {
 			return schemaContributor.requestCustomSchema(resource);
 		});
-		client.onRequest(CUSTOM_CONTENT_REQUEST, (uri) => {
+		client.onRequest(CUSTOM_CONTENT_REQUEST, uri => {
 			return schemaContributor.requestCustomSchemaContent(uri);
 		});
 	});
@@ -87,19 +130,34 @@ function getSchemaAssociation(context: ExtensionContext): ISchemaAssociations {
 	let associations: ISchemaAssociations = {};
 	extensions.all.forEach(extension => {
 		let packageJSON = extension.packageJSON;
-		if (packageJSON && packageJSON.contributes && packageJSON.contributes.yamlValidation) {
+		if (
+			packageJSON &&
+			packageJSON.contributes &&
+			packageJSON.contributes.yamlValidation
+		) {
 			let yamlValidation = packageJSON.contributes.yamlValidation;
 			if (Array.isArray(yamlValidation)) {
 				yamlValidation.forEach(jv => {
 					let { fileMatch, url } = jv;
 					if (fileMatch && url) {
 						if (url[0] === '.' && url[1] === '/') {
-							url = Uri.file(path.join(extension.extensionPath, url)).toString();
+							url = Uri.file(
+								path.join(extension.extensionPath, url)
+							).toString();
 						}
 						if (fileMatch[0] === '%') {
-							fileMatch = fileMatch.replace(/%APP_SETTINGS_HOME%/, '/User');
-							fileMatch = fileMatch.replace(/%APP_WORKSPACES_HOME%/, '/Workspaces');
-						} else if (fileMatch.charAt(0) !== '/' && !fileMatch.match(/\w+:\/\//)) {
+							fileMatch = fileMatch.replace(
+								/%APP_SETTINGS_HOME%/,
+								'/User'
+							);
+							fileMatch = fileMatch.replace(
+								/%APP_WORKSPACES_HOME%/,
+								'/Workspaces'
+							);
+						} else if (
+							fileMatch.charAt(0) !== '/' &&
+							!fileMatch.match(/\w+:\/\//)
+						) {
 							fileMatch = '/' + fileMatch;
 						}
 						let association = associations[fileMatch];
