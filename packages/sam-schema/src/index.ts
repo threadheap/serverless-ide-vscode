1380e2a7f80ebfaa2975be2ecfb1ef0d2590b0f4
Pavel Vlasov <freakycue@gmail.com>
2019-02-16 17:17:14 +1100

    new schema + license updates

--- packages/sam-schema/src/index.ts
+++ packages/sam-schema/src/index.ts
@@ -1,128 +1,159 @@
-"use strict";
+'use strict';
 
-import request = require("request-promise");
-import fs = require("fs");
-import path = require("path");
-import pick = require("lodash/pick");
+import request = require('request-promise');
+import fs = require('fs');
+import path = require('path');
+import cloneDeep = require('lodash/cloneDeep');
+import pick = require('lodash/pick');
+import stringify = require('json-stable-stringify');
 
-const downloadSchema = async (): Promise<object> => {
-  const response = await request.get(
-    "https://raw.githubusercontent.com/awslabs/goformation/master/schema/sam.schema.json",
-    { json: true }
-  );
+const cfnSchemaUrl =
+	'https://raw.githubusercontent.com/awslabs/goformation/master/schema/cloudformation.schema.json';
+const samSchemaUrl =
+	'https://raw.githubusercontent.com/awslabs/serverless-application-model/master/samtranslator/validator/sam_schema/schema.json';
 
-  return response;
-};
+const cfnResourceDefinition = 'CloudFormationResource';
+const cfnResourceDefinitionLink = `#/definitions/${cfnResourceDefinition}`;
 
 // https://github.com/awslabs/serverless-application-model/blob/master/docs/globals.rst
 // supported properties
 const globalFunctionProperties = [
-  "Handler",
-  "Runtime",
-  // Global CodeUri is not yet supported by cloudformation package
-  "CodeUri",
-  "DeadLetterQueue",
-  "Description",
-  "MemorySize",
-  "Timeout",
-  "VpcConfig",
-  "Environment",
-  "Tags",
-  "Tracing",
-  "KmsKeyArn",
-  "Layers",
-  "AutoPublishAlias",
-  "DeploymentPreference"
+	'Handler',
+	'Runtime',
+	// Global CodeUri is not yet supported by cloudformation package
+	'CodeUri',
+	'DeadLetterQueue',
+	'Description',
+	'MemorySize',
+	'Timeout',
+	'VpcConfig',
+	'Environment',
+	'Tags',
+	'Tracing',
+	'KmsKeyArn',
+	'Layers',
+	'AutoPublishAlias',
+	'DeploymentPreference'
 ];
 
 const globalApiProperties = [
-  "Name",
-  "DefinitionUri",
-  "CacheClusterEnabled",
-  "CacheClusterSize",
-  "Variables",
-  "EndpointConfiguration",
-  "MethodSettings",
-  "BinaryMediaTypes",
-  "Cors"
+	'Name',
+	'DefinitionUri',
+	'CacheClusterEnabled',
+	'CacheClusterSize',
+	'Variables',
+	'EndpointConfiguration',
+	'MethodSettings',
+	'BinaryMediaTypes',
+	'Cors'
 ];
 
-const globalTableProperties = ["SSESpecification"];
+const globalTableProperties = ['SSESpecification'];
 
-const generateSchema = (schema: any): any => {
-  if (!schema.properties.Globals) {
-    const functionDefinition = schema.definitions["AWS::Serverless::Function"];
-    const functionProperties =
-      functionDefinition.properties.Properties.properties;
-    const apiDefinition = schema.definitions["AWS::Serverless::Api"];
-    const apiProperties = apiDefinition.properties.Properties.properties;
-    const tableDefinition = schema.definitions["AWS::Serverless::SimpleTable"];
-    const tableProperties = tableDefinition.properties.Properties.properties;
+const downloadSchema = async (url: string): Promise<object> => {
+	const response = await request.get(url, { json: true });
 
-    // Global CodeUri is not yet supported by cloudformation package
-    // functionDefinition.required = functionDefinition.required.filter(
-    //     (option: string) => option !== 'CodeUri'
-    // );
+	return response;
+};
 
-    schema.properties.Globals = {
-      type: "object",
-      additionalProperties: false,
-      properties: {
-        Api: {
-          type: "object",
-          additionalProperties: false,
-          properties: {
-            EndpointConfiguration: {
-              $ref:
-                "#/definitions/AWS::ApiGateway::DomainName.EndpointConfiguration"
-            },
-            MethodSettings: {
-              items: {
-                $ref: "#/definitions/AWS::ApiGateway::Stage.MethodSetting"
-              },
-              type: "array"
-            },
-            BinaryMediaTypes: {
-              items: {
-                type: "string"
-              },
-              type: "array"
-            },
-            ...pick(apiProperties, globalApiProperties)
-          }
-        },
-        Function: {
-          type: "object",
-          additionalProperties: false,
-          properties: {
-            ...pick(functionProperties, globalFunctionProperties)
-          }
-        },
-        SimpleTable: {
-          type: "object",
-          additionalProperties: false,
-          properties: {
-            SSESpecification: {
-              $ref: "#/definitions/AWS::DAX::Cluster.SSESpecification"
-            },
-            ...pick(tableProperties, globalTableProperties)
-          }
-        }
-      }
-    };
-  }
+const mergeSchemas = (cfnSchema: any, samSchema: any): any => {
+	const functionDefinition =
+		samSchema.definitions['AWS::Serverless::Function'];
+	const functionProperties = cloneDeep(
+		functionDefinition.properties.Properties
+	);
+	const apiDefinition = samSchema.definitions['AWS::Serverless::Api'];
+	const apiProperties = apiDefinition.properties.Properties.properties;
+	const tableDefinition =
+		samSchema.definitions['AWS::Serverless::SimpleTable'];
+	const tableProperties = tableDefinition.properties.Properties.properties;
 
-  return schema;
+	const schema = cloneDeep(cfnSchema);
+
+	schema.definitions = Object.assign(
+		schema.definitions,
+		samSchema.definitions
+	);
+
+	delete schema.definitions[cfnResourceDefinition];
+
+	let cfnResources =
+		schema.properties.Resources.patternProperties['^[a-zA-Z0-9]+$'].anyOf;
+	const samResources =
+		samSchema.properties.Resources.patternProperties['^[a-zA-Z0-9]+$']
+			.anyOf;
+
+	samResources.map((resource: any) => {
+		if (resource.$ref !== cfnResourceDefinitionLink) {
+			cfnResources.push(resource);
+		}
+	});
+
+	// Globals
+	delete functionProperties.allOf[1].required;
+
+	schema.properties.Globals = {
+		type: 'object',
+		additionalProperties: false,
+		properties: {
+			Api: {
+				type: 'object',
+				additionalProperties: false,
+				properties: {
+					EndpointConfiguration: {
+						$ref:
+							'#/definitions/AWS::ApiGateway::DomainName.EndpointConfiguration'
+					},
+					MethodSettings: {
+						items: {
+							$ref:
+								'#/definitions/AWS::ApiGateway::Stage.MethodSetting'
+						},
+						type: 'array'
+					},
+					BinaryMediaTypes: {
+						items: {
+							type: 'string'
+						},
+						type: 'array'
+					},
+					...pick(apiProperties, globalApiProperties)
+				}
+			},
+			Function: {
+				type: 'object',
+				additionalProperties: false,
+				...functionProperties
+			},
+			SimpleTable: {
+				type: 'object',
+				additionalProperties: false,
+				properties: {
+					SSESpecification: {
+						$ref: '#/definitions/AWS::DAX::Cluster.SSESpecification'
+					},
+					...pick(tableProperties, globalTableProperties)
+				}
+			}
+		}
+	};
+
+	if (schema.required.indexOf('Transform') === -1) {
+		schema.required.push('Transform');
+	}
+
+	return schema;
 };
 
 const main = async () => {
-  const schema = await downloadSchema();
+	const cfnSchema = await downloadSchema(cfnSchemaUrl);
+	const samSchema = await downloadSchema(samSchemaUrl);
 
-  fs.writeFileSync(
-    path.join(process.cwd(), "schema.json"),
-    JSON.stringify(generateSchema(schema), null, 4),
-    "utf-8"
-  );
+	fs.writeFileSync(
+		path.join(process.cwd(), 'schema.json'),
+		stringify(mergeSchemas(cfnSchema, samSchema), { space: 4 }),
+		'utf-8'
+	);
 };
 
 main();
