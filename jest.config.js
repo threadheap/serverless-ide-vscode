4360049e1c87b04e9ae2e6af5c49924cec22a1b9
Pavel Vlasov <freakycue@gmail.com>
2019-03-03 12:04:54 +1100

    pre commit hooks

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	roots: [
+		'<rootDir>/packages/language-server',
+		'<rootDir>/packages/vscode',
+		'<rootDir>/packages/sam-schema'
+	],
+	testMatch: ['**/__tests__/**/?(*.)+(spec|test).+(ts|js)'],
+	modulePathIgnorePatterns: ['node_modules'],
+	preset: 'ts-jest',
+	testEnvironment: 'node'
+};
--- packages/language-server/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	roots: ['<rootDir>/src'],
-	testMatch: ['**/__tests__/**/?(*.)+(spec|test).+(ts|js)'],
-	preset: 'ts-jest',
-	testEnvironment: 'node'
-};
