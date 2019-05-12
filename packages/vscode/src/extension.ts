c4d3c6994e162b1d5e4e7a2fff8a967e0903271a
Pavel Vlasov <freakycue@gmail.com>
2019-05-12 15:40:42 +1000

    add activate/deactivate telementry

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -9,13 +9,21 @@ import {
 	ServerOptions,
 	TransportKind
 } from "vscode-languageclient"
+import * as packageJSON from "../package.json"
 import { schemaContributor } from "./schema-contributor"
+import { createReporter } from "./telemetry"
 
 export interface ISchemaAssociations {
 	[pattern: string]: string[]
 }
 
 let client: LanguageClient
+
+const telemetry = createReporter(
+	`${packageJSON.publisher}.${packageJSON.name}`,
+	packageJSON.version,
+	"e61496d8-12ad-43d9-bb32-cf41d527fdf0"
+)
 
 export function activate(context: ExtensionContext) {
 	const serverModule = context.asAbsolutePath(
@@ -65,10 +73,13 @@ export function activate(context: ExtensionContext) {
 	const disposable = client.start()
 
 	context.subscriptions.push(disposable)
+	context.subscriptions.push(telemetry.reporter)
 
 	languages.setLanguageConfiguration("yaml", {
 		wordPattern: /("(?:[^\\\"]*(?:\\.)?)*"?)|[^\s{}\[\],:]+/
 	})
+
+	telemetry.sendServerEvent("activated")
 
 	return schemaContributor
 }
@@ -77,5 +88,7 @@ export function deactivate(): Promise<void> | void {
 	if (!client) {
 		return undefined
 	}
+	telemetry.sendServerEvent("deactivated")
+	telemetry.reporter.dispose()
 	return client.stop() as Promise<void>
 }
---
+++ packages/vscode/src/telemetry/index.ts
@@ -0,0 +1,48 @@
+import TelemetryReporter from "vscode-extension-telemetry"
+
+class Telemetry {
+	public extensionId: string
+	public extensionVersion: string
+	private telemetryReporter: TelemetryReporter
+
+	get reporter(): TelemetryReporter {
+		return this.telemetryReporter
+	}
+
+	constructor(
+		extensionId: string,
+		extensionVersion: string,
+		instrumentationKey: string
+	) {
+		this.extensionId = extensionId
+		this.extensionVersion = extensionVersion
+		this.telemetryReporter = new TelemetryReporter(
+			extensionId,
+			extensionVersion,
+			instrumentationKey
+		)
+	}
+
+	public async sendServerEvent(
+		name: string,
+		stringData: { [key: string]: string } = {},
+		numericData: { [key: string]: number } = {}
+	): Promise<void> {
+		await this.reporter.sendTelemetryEvent(
+			name,
+			{
+				...stringData,
+				version: this.extensionVersion
+			},
+			numericData
+		)
+	}
+}
+
+export const createReporter = (
+	extensionId: string,
+	extensionVersion: string,
+	instrumentationKey: string
+): Telemetry => {
+	return new Telemetry(extensionId, extensionVersion, instrumentationKey)
+}
